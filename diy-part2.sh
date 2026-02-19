20cb26439e0d30879cc58ef84b377d0410ff96b0
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-02-19 18:20:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,14 @@
-#!/bin/bash
-# ... (保留原有的注释)
-
-# 1. 修改默认 IP (可选，如果需要将 1.1 改为其他，取消下面注释)
+# 1. 修改默认 IP (192.168.1.1 -> 192.168.10.1)
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改默认主题为 Design (核心修改)
-# 这行命令会将 LuCI 默认集合中的 bootstrap 替换为 design
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+# 2. 修改默认主题为 Argon
+# 首先，将 Makefile 中的默认主题从 bootstrap 改为 argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 3. 手动设置 MT7986A CPU 频率显示 (你原有的)
+# 3. 强制设置 Argon 为默认主题 (防止某些源码强制指定 bootstrap)
+# 这行命令会修改默认配置，确保系统启动后直接进入 Argon 界面
+echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
+
+# 4. 手动设置 MT7986A CPU 频率显示
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
-
-# 4. 优化：移除重复的主题（可选，防止固件臃肿）
-# rm -rf feeds/luci/themes/luci-theme-argon
-# rm -rf feeds/luci/themes/luci-theme-bootstrap
-
-# 5. 修复：确保 Lucky 插件能够编译 (针对某些源码环境)
-# 如果编译时发现缺少 lucky，取消下面这行的注释
-# git clone https://github.com/gandalf15/luci-app-lucky.git package/lucky
