2bfdbf4ffa95193e05919edc1c2510e751c4cbad
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-19 13:01:57 +0800

    Update diy-part1.sh

--- 24.10-6.6/diy-part1.sh
+++ 24.10-6.6/diy-part1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# 移除可能存在的旧条目
+sed -i '/nikki/d' feeds.conf.default
 
-# 1. 添加 Nikki 插件源 (以便随时可以编译本体)
-echo "src-git nikki https://github.com/houzi7141/openwrt-nikki.git" >> "feeds.conf.default"
+# 添加 Nikki 官方源
+echo "src-git nikki https://github.com/houzi7141/openwrt-nikki.git" >> feeds.conf.default
+
 
 # 2. 如果你想用最新版的 Argon 主题，也可以取消下面这两行的注释来拉取最新源码
 rm -rf package/lean/luci-theme-argon
