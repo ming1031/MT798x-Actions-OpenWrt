fce046e233e17d816573424cfbcfbed77cb0a7d9
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-30 02:14:21 +0800

    Update diy-part1.sh

--- 24.10-6.6/diy-part1.sh
+++ 24.10-6.6/diy-part1.sh
@@ -1,11 +1,14 @@
-# 进入 package 目录
-# 注意：如果你的脚本是在 openwrt 根目录执行的，请确保路径正确
-# 建议在脚本里先 cd package
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-# 1. 克隆修复版 Argon 主题
-rm -rf lean/luci-theme-argon # 如果有旧的先删掉
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git lean/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git lean/luci-app-argon-config
+# 1. 添加额外的插件仓库（可选）
+# 如果你除了 Lucky 还需要更多插件（比如常用的各种工具），可以解禁下面这一行
+# echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
 
-# 2. 克隆 Lucky 插件
-git clone https://github.com/gdy666/luci-app-lucky.git lean/luci-app-lucky
+# 2. 如果你在编译时需要特定的软件包源，可以在这里添加
+# 例如某些特定的软件仓库
