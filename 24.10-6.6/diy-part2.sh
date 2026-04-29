7a1c17783102dcfcb519678d64789980518ee85b
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-30 02:10:50 +0800

    Update diy-part2.sh

--- 24.10-6.6/diy-part2.sh
+++ 24.10-6.6/diy-part2.sh
@@ -3,10 +3,3 @@
 
 # 1. 修改默认 IP 为 10.10.10.1
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-
-# 2. 补齐 Nikki 运行所需的最后几个小包（不含本体）
-cat >> .config <<EOF
-CONFIG_PACKAGE_nftables-json=y
-CONFIG_PACKAGE_ip-full=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-EOF
