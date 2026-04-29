54ab834c8c040256722e7092473f5fd4e021bad8
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-30 02:12:45 +0800

    Update diy-part2.sh

--- 24.10-6.6/diy-part2.sh
+++ 24.10-6.6/diy-part2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# 1. 修改默认 IP 为 10.10.10.1
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 1. 下载 Argon 主题及其配置插件 (使用 master 分支兼容 24.10)
+# 先删除可能存在的旧目录
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-argon-config
+
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+
+# 2. 下载 Lucky 插件
+rm -rf package/lean/luci-app-lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lean/luci-app-lucky
