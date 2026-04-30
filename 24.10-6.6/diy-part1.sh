02d6c5c20519d0cdf6ff800e69c859a07db56516
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-30 09:14:12 +0800

    Update diy-part1.sh

--- 24.10-6.6/diy-part1.sh
+++ 24.10-6.6/diy-part1.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# 1. 添加额外的插件仓库（可选）
-# 如果你除了 Lucky 还需要更多插件（比如常用的各种工具），可以解禁下面这一行
-# echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-
-# 2. 如果你在编译时需要特定的软件包源，可以在这里添加
-# 例如某些特定的软件仓库
+exit 0
