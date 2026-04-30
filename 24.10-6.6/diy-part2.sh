458ec30cb8e88e0970265066b577c68f1cf81b5c
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-04-30 09:05:04 +0800

    Update diy-part2.sh

--- 24.10-6.6/diy-part2.sh
+++ 24.10-6.6/diy-part2.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# 1. 修正后台 IP
-sed -i "s/192.168.1.1/$IP_ADDR/g" package/base-files/files/bin/config_generate
+# 1. 克隆兼容 24.10 的 Argon 主题 (master 分支)
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-argon-config
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
-# 2. 克隆兼容 24.10 的 Argon 主题
-rm -rf package/feeds/luci/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
-# 建议直接放到 package 目录下最稳妥
-git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-
-# 3. 克隆 Lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
+# 2. 克隆 Lucky
+rm -rf package/lean/luci-app-lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lean/luci-app-lucky
