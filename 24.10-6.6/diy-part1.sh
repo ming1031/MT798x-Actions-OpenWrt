ac3060c0d2b8b925881f5fd9af0500df4a9b6ef5
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-05-03 20:39:35 +0800

    Update diy-part1.sh

--- 24.10-6.6/diy-part1.sh
+++ 24.10-6.6/diy-part1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 修改默认 IP (192.168.6.1 -> 10.10.10.1)  
+# sed -i 's/192.168.6.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+
 # 手动设置 MT7986A CPU 频率显示  
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
 exit 0
