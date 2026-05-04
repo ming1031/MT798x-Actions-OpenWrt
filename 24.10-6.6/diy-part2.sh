26339ebe7320d2fc0ba52ff5cf5718e0d85e8b2f
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-05-04 16:45:31 +0800

    Update diy-part2.sh

--- 24.10-6.6/diy-part2.sh
+++ 24.10-6.6/diy-part2.sh
@@ -6,4 +6,4 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 git clone https://github.com/gdy666/luci-app-lucky package/lucky
 
 # Openclash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone --depth 1 -b dev https://github.com/vernesong/OpenClash.git package/luci-app-openclash
