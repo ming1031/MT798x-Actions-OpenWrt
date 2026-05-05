f2c7f257e7c34c681ba34a68d5469662ac71dfa8
ming1031 <99428158+ming1031@users.noreply.github.com>
2026-05-05 11:10:02 +0800

    Update diy-part2.sh

--- 24.10-6.6/diy-part2.sh
+++ 24.10-6.6/diy-part2.sh
@@ -1,6 +1,5 @@
 # Argon（新版兼容24.10）
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # Lucky
 git clone https://github.com/gdy666/luci-app-lucky package/lucky
